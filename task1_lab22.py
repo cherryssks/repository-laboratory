7fb9ab60c611fb81e61e833e220269be7e75e299
cherryssks <cherryssks@gmail.com>
2025-02-03 20:21:12 +0300

    Лабораторная работа 2

---
+++ task1_lab22.py
@@ -0,0 +1,11 @@
+class Book:
+    def __init__(self, id_, name, pages):
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f'Book(id_={self.id}, name={repr(self.name)}, pages={self.pages})'
---
+++ task2_lab22.py
@@ -0,0 +1,17 @@
+class Library:
+    def __init__(self, books=None):
+        if books is None:
+            books = []
+        self.books = books
+
+    def get_next_book_id(self):
+        if not self.books:
+            return 1
+        else:
+            return self.books[-1].id + 1
+
+    def get_index_by_book_id(self, book_id):
+        for index, book in enumerate(self.books):
+            if book.id == book_id:
+                return index
+        raise ValueError("Книги с запрашиваемым id не существует")
